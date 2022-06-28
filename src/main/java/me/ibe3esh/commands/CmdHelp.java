4001cc1d8fb606ac90935a112aeab4657b8be2ce
Ibe3esh <61380002+Ibe3esh@users.noreply.github.com>
2022-06-28 02:35:57 +0200

    Changed CmdHe

--- src/main/java/me/ibe3esh/commands/CmdHelp.java
+++ src/main/java/me/ibe3esh/commands/CmdHelp.java
@@ -7,16 +7,16 @@ public class CmdHelp implements ICommand {
 
     @Override
     public void execute(ExecuteArgs event) {
-        event.getTextChannel().sendMessage("`!avatar - This command is to show avatars.`");
-        event.getTextChannel().sendMessage("`!help - Shows the list with commands in the bot.`");
-        event.getTextChannel().sendMessage("`!profile - This command is to show profiles.`");
+        event.getTextChannel().sendMessage("`!avatar - This command is to show avatars.`").queue();
+        event.getTextChannel().sendMessage("`!help - Shows the list with commands in the bot.`").queue();
+        event.getTextChannel().sendMessage("`!profile - This command is to show profiles.`").queue();
 
-        event.getTextChannel().sendMessage("`!nowplaying - Shows the currently playing song.`");
-        event.getTextChannel().sendMessage("`!play - This command is to play music.`");
-        event.getTextChannel().sendMessage("`!queue - Shows the queued up songs.`");
-        event.getTextChannel().sendMessage("`!repeat - Loops the current song.`");
-        event.getTextChannel().sendMessage("`!skip - Skips to current track.`");
-        event.getTextChannel().sendMessage("`!stop - Stops the current song and clears the queue.`");
+        event.getTextChannel().sendMessage("`!nowplaying - Shows the currently playing song.`").queue();
+        event.getTextChannel().sendMessage("`!play - This command is to play music.`").queue();
+        event.getTextChannel().sendMessage("`!queue - Shows the queued up songs.`").queue();
+        event.getTextChannel().sendMessage("`!repeat - Loops the current song.`").queue();
+        event.getTextChannel().sendMessage("`!skip - Skips to current track.`").queue();
+        event.getTextChannel().sendMessage("`!stop - Stops the current song and clears the queue.`").queue();
     }
 
     @Override
